68bb70406f3f926f5ce7480ce680e4b217c9f535
Purodha <purodha@blissenbach.org>
2015-11-07 02:38:21 +0000

    Show "redirected from..." headers on OW pages
    This leaves T118066 open (red links for valid non-canonical DM page names)
    
    Bug: T64489
    Change-Id: I01126069431b0fc1613b7b9853a2981e8b354c93

--- includes/WikidataArticle.php
+++ includes/WikidataArticle.php
@@ -9,6 +9,7 @@ class WikidataArticle extends Article {
 		$ns = $this->mTitle->getNamespace();
 		$handlerClass = $wdHandlerClasses[ $ns ];
 		$handlerInstance = new $handlerClass( $this->mTitle );
+		$this->showRedirectedFromHeader();
 		$handlerInstance->view();
 
 		wfProfileOut( __METHOD__ );
